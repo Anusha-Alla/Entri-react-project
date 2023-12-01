16ef6dd2381ecc9892a223cdceca6eef2d8d72ee
Anusha <kollianu1992@gmail.com>
2023-12-01 15:37:35 +0530

    Src and it's files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+
+import Navbar from './Navbar';
+import Carousel from './Carousel';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
